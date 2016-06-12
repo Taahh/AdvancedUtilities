be31c67eee3154816b8a6ac608a308f8bfe6385a
ThePheonixBlader <thepheonixblader@gmail.com>
2016-06-11 19:20:46 -0700

    First commit

---
+++ src/me/taahanis/AdvancedUtilities/AdvancedUtils.java
@@ -0,0 +1,25 @@
+package me.taahanis.AdvancedUtilities;
+
+import net.pravian.aero.plugin.AeroPlugin;
+
+public class AdvancedUtils extends AeroPlugin<AdvancedUtils> {
+    
+    public static AdvancedUtils plugin;
+    
+    @Override
+    public void load() {
+        
+    }
+
+    @Override
+    public void enable() {
+        
+    }
+
+    @Override
+    public void disable() {
+        
+    }
+
+    
+}
